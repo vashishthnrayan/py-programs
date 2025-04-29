5e20f8e5084d1fb4e7ff91cada9ed543ddcf944d
vashishthnrayan <vedmedicare123@gmail.com>
2025-04-29 17:31:30 +0530

    commit 1

---
+++ module 1/Activity.py
@@ -0,0 +1,27 @@
+print("Hello world")
+
+
+# all the arthematic operater
+a =3
+b = 5
+print("the sum of a + b =",a+b)
+print("the sum of a - b =",a-b)
+print("the sum of a * b =",a*b)
+print("the sum of a / b =",b/a)
+
+
+print(type(a))
+
+
+# a, b = 1, "2"
+# c = int(b)
+# sum = a + c
+
+
+age= 12 
+print("I am",age)
+
+print(4)
+print("Hello python \n")
+print("hello ",5)
+print("All the best for this course",end="👍")
