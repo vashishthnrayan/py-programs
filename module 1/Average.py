9136bc841b584589119ac011e1dff1ec2f84ce8b
vashishthnrayan <vedmedicare123@gmail.com>
2025-05-06 17:23:25 +0530

    average.py

---
+++ module 1/Average.py
@@ -0,0 +1,24 @@
+# hi= 5 == int(5)
+# print(hi)
+
+# x=3
+# x=x+1
+# print(x)
+# x+=10
+
+
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+
+sum=tree1+tree2+tree3+tree4+tree5
+print("Sum of all the 5 trees is:", sum)
+
+# finding the average of the 5 trees
+# average=sum of all the 5 trees/sum of observations
+average=sum/5
+print("Average of all the 5 trees is:", average)
+
+
