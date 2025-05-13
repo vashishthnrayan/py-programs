9c7e39a41cd33f56304cb33718f114034e039f36
vashishthnrayan <vedmedicare123@gmail.com>
2025-05-13 17:56:18 +0530

    and-or_operater

---
+++ module 1/and-or_operater.py
@@ -0,0 +1,20 @@
+x =int(input("Enter the value of x: "))
+y =int(input("Enter the value of y: "))
+z =int(input("Enter the value of z: "))
+if (x > y) and (x > z):
+    print("x is greater ")
+elif (y>x) and (y > z):
+    print("y is greater")
+else:
+    print  ("z is greater")
+
+
+
+a=10
+
+y=-10
+
+if a>0 or y>0:
+    print("Atleast one number is positive")
+else:
+    print("Both numbers are negative")
