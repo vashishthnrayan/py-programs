312171c90be02d148992d8f0522987d41475982f
vashishthnrayan <vedmedicare123@gmail.com>
2025-05-13 17:30:50 +0530

    elif

---
+++ module 1/elif.py
@@ -0,0 +1,8 @@
+print("Number is positive , negitive  or Zero")
+num = int(input("Enter a number: "))
+if num > 0:
+    print(num, "is a positive number.")
+elif num < 0:
+    print(num, "is a negitive number.")
+else:
+    print(num, "is zero.")
