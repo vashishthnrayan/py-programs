dbd3397a0249a4c012f0bdb8aa37bcb0bcc1e612
vashishthnrayan <vedmedicare123@gmail.com>
2025-05-20 17:39:32 +0530

    Reverse a String

---
+++ module 1/Reverse a String.py
@@ -0,0 +1,7 @@
+string= input("plz enter your own string: ")
+string2=('')
+
+for i in string:
+    string2= i +string2
+print("\n the original string =", string)
+print("\n the reversed string =", string2)
