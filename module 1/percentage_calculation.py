f8ebc9bdb9577473c616970c92599ffe741b9cae
vashishthnrayan <vedmedicare123@gmail.com>
2025-05-20 17:30:17 +0530

    Sum of whole numbers

--- module 1/percentage_calculation.py
+++ module 1/percentage_calculation.py
@@ -12,8 +12,3 @@ print(end="Percentage Mark = ")
 print( perc,"%")
 
 
-# sq_root
-print("Square Root of a number: ")
-number = int(input("Enter Number: "))
-Sq_root = number ** 0.5
-print ("Square root of", number, "is", Sq_root)
