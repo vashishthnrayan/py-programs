9e0d84df18be8dd784247e1f642dbea1a7b6c415
vashishthnrayan <vedmedicare123@gmail.com>
2025-05-22 17:31:32 +0530

    Sum of Natural Numbers

---
+++ module 1/Sum of Natural Numbers.py
@@ -0,0 +1,12 @@
+print("while loop")
+n=int(input("Enter a number: ")) #initializing variable
+while (n>=0): #condition
+    print(n) #printing the value of n
+    n-=1 #decrementing the value of n
+
+print("counting From 0 to 5\n")
+
+i=0  #initializing variable
+while(i<5):
+    print(i) #printing the value of i
+    i+=1 #incrementing the value of i
