47c881d841c7d289a15d1fab0d734ca0ac973b51
vashishthnrayan <vedmedicare123@gmail.com>
2025-05-22 17:40:31 +0530

    Sum of Natural Numbers

---
+++ module 1/Sum of Natural Numbers.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a number: ")) #initializing variable
+
+sum=0 #initializing sum variable
+i=1 #initializing i variable
+while i<=n: #condition
+    sum+=i #adding i to sum
+    i+=1 #incrementing the value of i
+    print(sum) #printing the value of sum
+    print("\nSum of first",n,"natural numbers =",sum) #printing the sum of first n natural numbers10
