c0933ea2bd5a48efbef5885b2e0542f3fd4d13ff
vashishthnrayan <vedmedicare123@gmail.com>
2025-06-05 18:13:29 +0530

    c

--- module 1/Floyd's triangle.py
+++ module 1/Floyd's triangle.py
@@ -3,6 +3,6 @@ n=int(input("Enter the number of rows: "))
 number =1
 for i in range(1,n+1):
     for j in range(0,i):
-        print(number,end="      " ,flush=True)
+        print(number,end=" " ,flush=True)
         number += 1
     print()  # Move to the next line after each row
--- module 1/reverse_code.py
+++ module 1/reverse_code.py
@@ -1,6 +1,6 @@
 for i in range(1, 11):
      print(i)
 n=int(input("enter the value of n\n"))
-print("number from {0}to{1}are:".format(n, 1))
+print("number from {0} to {1} are:".format(n, 1))
 for i in range(n, 0, -1):
     print(i)
