ad589c8a0b3ef797537a6f166d539ec254eb1908
vashishthnrayan <vedmedicare123@gmail.com>
2025-06-12 17:39:04 +0530

    countinue

---
+++ module 1/countinue.py
@@ -0,0 +1,7 @@
+var =11
+while var > 0: #while loop
+    var -= 1 #decrement variable
+    if var == 5: #condition
+        continue #continue statement
+    print("Current variable value:", var) #display result
+print("\nGood bye!")
