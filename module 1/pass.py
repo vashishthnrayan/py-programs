831eb8c08203de0c96ed51c38c79cc6b0fd5a6ae
vashishthnrayan <vedmedicare123@gmail.com>
2025-06-12 17:34:09 +0530

    pass

--- module 1/pass.py
+++ module 1/pass.py
@@ -1,7 +1,7 @@
 for x in range(10): #iterate for loop
     if x % 20 == 0: #condition 1
         print("twist")
-    elif x % 15 == 0: #condition 2
+    elif x % 2 == 0: #condition 2
         pass
 #pass statement
     elif x % 5 == 0: #condition 3
