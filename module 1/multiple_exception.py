f5939d619ba49cd5184e8c67106fcf78aa5743f3
vashishthnrayan <vedmedicare123@gmail.com>
2025-06-17 17:28:37 +0530

    multiple_exception

---
+++ module 1/multiple_exception.py
@@ -0,0 +1,16 @@
+try:
+    num1,num2=eval(input("Enter two numbers,sperated by comma: "))
+    result = num1 / num2
+    print("The result is:", result)
+
+except ZeroDivisionError :
+    print("Error: Division by zero is not allowed.")
+except SyntaxError:
+    print("Error: Invalid input syntax. Please enter two numbers separated by a comma.")
+
+except :
+    print("wrong input")
+else:
+    print("no exception")
+finally:
+    print("this will execute no matter what")
