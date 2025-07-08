3eee49fe1e5dfe57d9d4a374e03c90808ec332c7
vashishthnrayan <vedmedicare123@gmail.com>
2025-07-08 17:53:35 +0530

    set intersection

--- module 1/set intersection.py
+++ module 1/set intersection.py
@@ -6,3 +6,9 @@ print(sety)
 print("\nIntersection of two said sets:")
 setz = setx.intersection(sety)
 print(setz)
+print(setx.union(sety))  # Union of two sets
+
+print(setx.difference(sety))    # Difference of two sets
+
+
+print(setx.symmetric_difference(sety))  
