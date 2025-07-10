5e9a5530a0247d291c7cfd8c4da5b6d070ea6f7d
vashishthnrayan <vedmedicare123@gmail.com>
2025-07-10 17:46:37 +0530

    exit it

---
+++ module 1/exit it .py
@@ -0,0 +1,8 @@
+ages=[19,45,12,78]
+for age in ages:
+    if age < 18:
+        print(age,"not allowed")
+        print(exit)
+        exit()
+    else:
+        print(age,"You are eligible to vote")
