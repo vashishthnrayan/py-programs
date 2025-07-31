68aad8cff2b09458725497628cba4e4bed1a511e
vashishthnrayan <vedmedicare123@gmail.com>
2025-07-31 17:28:31 +0530

    keep_it_private

---
+++ module 1/keep_it_private.py
@@ -0,0 +1,13 @@
+class Base:
+    def __init__ (self) :
+        self.a = "GeeksforGeeks"
+        self.__c = "GeeksforGeeks"
+    
+class Derived(Base):
+    def __init__ (self):
+        Base.__init__(self)
+        print("Calling private member of base class: ")
+        print(self._c)
+        
+obj1 =Base()
+print(obj1.a)
